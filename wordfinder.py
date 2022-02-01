9a14b9c1dfd22b383bf7bdf9d070ea706d940ada
Stephanie <smmackin12@gmail.com>
2022-02-01 15:47:07 -0500

    Word Finder

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,18 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    """
+    >>>words = WordFinder('words.txt')
+    
+    """
+    
+    def __init__ ( self, path ):
+        """initializes the path of words to use for word finder"""
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print (f"{len(self.words)} words read")
+        
+    def parse (self, dict_file):
+        return [word.strip() for word in dict_file]
